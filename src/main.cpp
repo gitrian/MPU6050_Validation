ebe3f94a381e3cfed17a4e349d15d0b241b089c1
Idontwantyourcloud <flolippert@t-online.de>
2019-02-21 18:36:10 +0100

    include MPU Libs

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
 #include <Arduino.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
 void setup() {
   // put your setup code here, to run once:
