4faba3891bc68c262858c747d4301467b3a155d7
Idontwantyourcloud <flolippert@t-online.de>
2019-03-30 13:51:49 +0100

    current running option. Need to be advanced.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <Arduino.h>
 #include <Arduino_impl.h>
 #include <math.h>
 #define PL(x) Serial.println(x)
@@ -11,42 +12,51 @@ void interrupt(){
   dataReady = true;
 }
 
+/* Declaration of Variables */
 double a, b, c, d, e, x, y, z = 0;
 long t1, t2, t3 = 0;
-
 unsigned short gyro_rate, gyro_fsr;
 unsigned char accel_fsr;
 float gyro_sens;
 unsigned short accel_sens;
 unsigned short rate;
-void setup() {
-  Serial.begin(115200);
-  while(!Serial);
-  struct int_param_s params;
-  params.pin = 2;
-  params.cb = interrupt;
-  PL(mpu_init(&params));
-  PL(mpu_set_sensors(INV_XYZ_GYRO | INV_XYZ_ACCEL));
-  PL(mpu_configure_fifo(INV_XYZ_GYRO | INV_XYZ_ACCEL));
-  //PL(mpu_set_gyro_fsr(10));
-  //PL(mpu_set_accel_fsr(4));
-  PL(mpu_set_sample_rate(50));
-  //PL(mpu_set_lpf(42));
-
-  PL(dmp_load_motion_driver_firmware());
-  PL(mpu_set_dmp_state(1));
-  PL(dmp_enable_feature(DMP_FEATURE_6X_LP_QUAT | DMP_FEATURE_SEND_CAL_GYRO));
-  PL(dmp_enable_6x_lp_quat(1));
-  PL(dmp_enable_gyro_cal(1));
-  PL(dmp_set_fifo_rate(50));
-}
-
 short gyro[3], accel[3], sensors;
 unsigned char more;
 long quat[4];
 unsigned long sensor_timestamp;
 
+int8_t MPU_Startup() {
+  int8_t configCount=0;
+  struct int_param_s params;
+  params.pin = 2;
+  params.cb = interrupt;
+
+  
+  if (mpu_init(&params))  configCount++;
+  if (mpu_set_sensors(INV_XYZ_GYRO | INV_XYZ_ACCEL))  configCount++;
+  if (mpu_configure_fifo(INV_XYZ_GYRO | INV_XYZ_ACCEL)) configCount++;
+  if (mpu_set_sample_rate(50))  configCount++;
+
+  if (dmp_load_motion_driver_firmware())  configCount++;
+  if (mpu_set_dmp_state(1)) configCount++;
+  if (dmp_enable_feature(DMP_FEATURE_6X_LP_QUAT | DMP_FEATURE_SEND_CAL_GYRO)) configCount++;
+  if (dmp_enable_6x_lp_quat(1)) configCount++;
+  if (dmp_enable_gyro_cal(1)) configCount++;
+  if (dmp_set_fifo_rate(50))  configCount++;
+
+
+  return configCount;
+}
+
+void setup() {
+  Serial.begin(115200);
+  while(!Serial);
+  if (MPU_Startup()==10)  Serial.println("jetzat machma do erstameu a heube auf!");
+  else Serial.println("ahhhh shit.");
+}
+
 void loop() {
+  MPU_Startup();
   if(dataReady){
     if (!more) dataReady=false;
     dmp_read_fifo(gyro, accel, quat, &sensor_timestamp, &sensors, &more);
